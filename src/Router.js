0ec4dea6a284a2b19f47d3ab4165ef4749bcf739
김정준 <jjkm0222@gmail.com>
2022-06-16 19:47:45 +0900

    conflict resolved

--- src/Router.js
+++ src/Router.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/main/Main';
 import Signup from './pages/signup/Signup';
-import Login from './pages/login/Login';
 import JobList from './pages/joblist/JobList';
 import JobDetail from './pages/jobdetail/JobDetail';
 import LikePage from './pages/likePage/LikePage';
@@ -21,7 +20,6 @@ const Router = () => {
         <Route path="/likePage" element={<LikePage />} />
         <Route path="/" element={<Main />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/joblist" element={<JobList />} />
         <Route path="/jobdetail/:company_id" element={<JobDetail />} />
         <Route path="/resume" element={<Resume />} />
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,5 +1,64 @@
+import React, { useEffect, useState } from 'react';
+
+import HeaderCarousel from './HeaderCarousel';
+import MidNavBar from './MidNavBar';
+import ApplyNowCarousel from './ApplyNowCarousel';
+import styled from 'styled-components';
+import SuggestCarousel from './SuggestCarousel';
+
 const Main = () => {
-  return <div>메인페이지입니다</div>;
+  const [companyData, setCompanyData] = useState([]);
+  useEffect(() => {
+    fetch('http://10.58.2.54:8000/jobs/random')
+      .then(res => res.json())
+      .then(data => {
+        setCompanyData(data.results);
+      });
+  }, []);
+
+  const [headerCompanyImg, setHeaderCompanyImg] = useState([]);
+  useEffect(() => {
+    fetch('/data/MainImg.json')
+      .then(res => res.json())
+      .then(data => {
+        setHeaderCompanyImg(data);
+      });
+  }, []);
+
+  return (
+    companyData[0] && (
+      <>
+        <HeaderMargin />
+        <HeaderCarousel headerCompanyImg={headerCompanyImg} />
+        <MidNavBar />
+        <SideMargin>
+          <Text>지금 바로 지원해볼까요?</Text>
+          <ApplyNowCarousel companyData={companyData} />
+        </SideMargin>
+        <SideMargin>
+          <Text>wanted ai가 제안하는 합격률 높은 포지션</Text>
+          <SuggestCarousel companyData={companyData} />
+        </SideMargin>
+      </>
+    )
+  );
 };
 
+const HeaderMargin = styled.div`
+  margin-top: 10rem;
+`;
+
+const SideMargin = styled.div`
+  margin: 0 20rem;
+`;
+
+const Text = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Fascinate&family=Lobster&display=swap');
+  display: flex;
+  justify-content: center;
+  margin: 8rem 0 2rem;
+  font-size: 2rem;
+  font-weight: bold;
+  font-family: 'Black Han Sans', sans-serif;
+`;
 export default Main;
